b67f4d7156ef4e9783ced339d18b7697709f6a3c
Adnan Anjum <anjumcode12@gmail.com>
2026-06-20 11:15:31 +0500

    add pakages configs

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { connectDB } from './db/index.js';
+import { app } from './app.js';
+import dotenv from 'dotenv';
+
+dotenv.config({
+  path: './.env',
+});
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log('Server Started at port:', process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log('Some error Occured', error);
+  });
