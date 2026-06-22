f7788224c353f312136bc64a7af6d255ba13c73b
Adnan Anjum <anjumcode12@gmail.com>
2026-06-23 01:24:52 +0500

    feat(company): add getAllCompanies controller

--- src/routes/company.routes.js
+++ src/routes/company.routes.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import { verifyJWT } from '../middleware/auth.middleware.js';
 import { upload } from '../middleware/multer.middleware.js';
-import { createCompany } from '../controllers/company.controller.js';
+import {
+  createCompany,
+  getAllCompanies,
+} from '../controllers/company.controller.js';
 
 const router = Router();
 router.route('/').post(verifyJWT, upload.single('logo'), createCompany);
+router.route('/').get(getAllCompanies);
 export default router;
