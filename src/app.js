653457aa93c4230fddf549ed1f87f31bfec61f50
Adnan Anjum <anjumcode12@gmail.com>
2026-06-24 01:00:52 +0500

    implemented jobs route mounting

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import userRouter from './routes/user.routes.js';
 import companyRouter from './routes/company.routes.js';
+import jobsRouter from './routes/jobs.routes.js';
 const app = express();
 
 app.use(
@@ -23,6 +24,8 @@ app.use('/api/v1/users', userRouter);
 //company routes
 app.use('/api/v1/companies', companyRouter);
 
+// jobs routes
+app.use('/api/v1/jobs', jobsRouter);
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   res.status(statusCode).json({
